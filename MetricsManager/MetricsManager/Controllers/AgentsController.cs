b435e92ea349c9dd54558829cbe3b93d1f0f222d
Павел Ремизов <remizov@rightway.games>
2021-04-24 19:59:54 +0300

    Практическое задание. Добавить метод в контроллер агентов проекта менеджера метрик позволяющий получить список зарегистрированных в системе объектов

--- MetricsManager/MetricsManager/Controllers/AgentsController.cs
+++ MetricsManager/MetricsManager/Controllers/AgentsController.cs
@@ -7,6 +7,13 @@ namespace MetricsManager.Controllers
     [ApiController]
     public class AgentsController : ControllerBase
     {
+        private readonly AgentsModel _agentsModel;
+
+        public AgentsController(AgentsModel agentsModel)
+        {
+            _agentsModel = agentsModel;
+        }
+
         [HttpPost("register")]
         public IActionResult RegisterAgent([FromBody] AgentInfo agentInfo)
         {
@@ -22,11 +29,15 @@ namespace MetricsManager.Controllers
         {
             return Ok();
         }
-
         [HttpPut("disable/{agentId}")]
         public IActionResult DisableAgentById([FromRoute] int agentId)
         {
             return Ok();
+        }
+        [HttpGet("get_agents")]
+        public IActionResult GetRegisterAgents()
+        {
+            return Ok(_agentsModel.GetAgentsInfo());
         }
     }
 }
