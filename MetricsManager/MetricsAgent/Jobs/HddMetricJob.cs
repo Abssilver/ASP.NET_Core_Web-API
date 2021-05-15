693cc0051b52bb81eac6a1e2813dd9a367c79da3
Павел Ремизов <remizov@rightway.games>
2021-05-15 16:57:16 +0300

    Урок 5. Реализация задач для сбора метрик с помощью perfomance-counters. Интеграция FluentMigrator. Интеграция Quartz. Интеграция System.Diagnostics.PerformanceCounter

---
+++ MetricsManager/MetricsAgent/Jobs/HddMetricJob.cs
@@ -0,0 +1,34 @@
+﻿using Quartz;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using MetricsAgent.DataAccessLayer.Interfaces;
+using MetricsAgent.DataAccessLayer.Models;
+
+namespace MetricsAgent.Jobs
+{
+    public class HddMetricJob : IJob
+    {
+        private readonly IHddMetricsRepository _repository;
+        private readonly PerformanceCounter _counter;
+        
+        public HddMetricJob(IHddMetricsRepository repository)
+        {
+            _repository = repository;
+            _counter = new PerformanceCounter(
+                "PhysicalDisk", "% Disk Time", "_Total");
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            //disk activity %
+            var metrics = Convert.ToInt32(_counter.NextValue());
+            var time = DateTimeOffset.UtcNow;
+            
+            _repository.Create(new HddMetric { Time = time, Value = metrics });
+            
+            return Task.CompletedTask;
+        }
+
+    }
+}
